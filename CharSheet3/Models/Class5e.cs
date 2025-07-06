0c703b3622fe4157598aa8116d2d4f0dcfaa0705
GoldenFlameZero <goldenflamezero@gmail.com>
2025-07-07 09:24:40 +1000

    Add 5e class model. Do some cleanup.

---
+++ CharSheet3/Models/Class5e.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharSheet3.Models;
+
+public class Class5e
+{
+    public string Name { get; set; } = "New Class";
+    public int HitDie { get; set; } = 8; // Default to D8
+    public Dictionary<ClassFeature5e, int> Features { get; set; } = [];
+    public List<Subclass5e> Subclasses { get; set; } = [];
+}
+
+public class Subclass5e
+{
+    public string Name { get; set; } = "New Subclass";
+    public Dictionary<ClassFeature5e, int> Features { get; set; } = [];
+}
+
+public class  ClassFeature5e
+{
+    public string Name { get; set; } = "New Feature";
+    public string Description { get; set; } = "Feature description goes here.";
+}
+
